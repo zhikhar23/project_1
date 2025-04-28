01811575e08e775d1081023f1e4efa7efd911ca1
Max Zhikharev <zhikhar23@gmail.com>
2025-04-28 10:20:11 +0300

    внесены исправления

--- src/processing.py
+++ src/processing.py
@@ -6,6 +6,6 @@ def filter_by_state(transactions: List[Dict], state: str = "EXECUTED") -> List[D
     return [item for item in transactions if item.get("state") == state]
 
 
-def sort_by_date(transactions: List[Dict]) -> List[Dict]:
+def sort_by_date(transactions: List[Dict], ascending: bool = True) -> List[Dict]:
     """Функция для сортировки списка по дате"""
-    return sorted(transactions, key=lambda x: x["date"], reverse=True)
+    return sorted(transactions, key=lambda x: x["date"], reverse=ascending)
