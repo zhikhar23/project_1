9d7eedd5272d6614358fb3c45619bb2aff0757dc
Max Zhikharev <zhikhar23@gmail.com>
2025-06-09 19:02:12 +0300

    внесены корректировки к дз 12.1

---
+++ src/external_api_2.py
@@ -0,0 +1,36 @@
+import os
+import requests
+from dotenv import load_dotenv
+from src.utils import get_transactions
+
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+payload = {}
+headers = {"apikey": API_KEY}
+
+currency_code = "RUB"
+
+def transaction_summ(transact, code):
+    """Функция обрабатывающая транзакции и возвращающая сумму всех транзакций в рублях
+    если транзакция в USD или EUR ,то конвертирует их по курсу через API"""
+
+    if not transact.get("operationAmount") :
+        return "Неподходящая транзакция"
+
+    if transact["operationAmount"]["currency"].get("code"):
+        transaction_code = transact.get("operationAmount").get("currency").get("code")
+        amount = float(transact.get("operationAmount").get("amount"))
+
+        if transaction_code == code:
+            return float(transact["operationAmount"]["amount"])
+
+        elif transaction_code == "USD" or transaction_code == "EUR":
+              url = f"https://api.apilayer.com/exchangerates_data/convert?to={code}&from={transaction_code}&amount={amount}"
+              response = requests.get(url, headers=headers, data=payload)
+              if response.status_code == 200:
+                   result = response.json().get("result")
+                   if isinstance(result, (int, float)):
+                        return result
+              else:
+                    return f"Не успешный запрос, код ошибки: {response.status_code}"
--- tests/test_external.py
+++ tests/test_external.py
@@ -1,60 +1,50 @@
+import builtins
 import json
 import os
-
 import pytest
-
-from src.external_api import transaction_summ
-from unittest.mock import Mock
+from src.external_api_2 import transaction_summ
+from unittest.mock import Mock, mock_open
 from unittest.mock import patch
 import json
 from src.utils import get_transactions
 import pytest
 import requests
 
+
+
 def test_get_transaction():
     """Тестируем функция с помощью Моск"""
-    mock_2=Mock(return_value=[{'d':3},{"d":3}])
-    json.load=mock_2
-    assert get_transactions(json_path="../data/operations.json")==[{'d':3},{"d":3}]
-    mock_2.assert_called_once()
+    test_data = [{"id": 1, "amount": 100}]
+
+    os.path.exists = Mock(return_value=True)
+    builtins.open = mock_open(read_data=json.dumps(test_data))
+
+    result = get_transactions(" ")
+    assert result == test_data
+    builtins.open.assert_called_once()
 
 @pytest.fixture
-def transact_list():
-    return [{
+def transaction():
+    return {
     "id": 441945886,
     "state": "EXECUTED",
     "date": "2019-08-26T10:50:58.294041",
     "operationAmount": {
-      "amount": "1",
+      "amount": "100",
       "currency": {
-        "name": "руб.",
-        "code": "RUB"
+        "name": "дол.",
+        "code": "USD"
       }
     },
     "description": "Перевод организации",
     "from": "Maestro 1596837868705199",
     "to": "Счет 64686473678894779589"
-  },
-        {
-    "id": 939719570,
-    "state": "EXECUTED",
-    "date": "2018-06-30T02:08:58.425572",
-    "operationAmount": {
-      "amount": "100",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "Счет 75106830613657916952",
-    "to": "Счет 11776614605963066702"
   }
-]
+
 @patch('requests.get')
-def test_transact_summ(mock_get,transact_list):
+def test_transact_summ(mock_get,transaction):
     """Тестируем с помощью patch"""
     mock_response=mock_get.return_value
     mock_response.status_code=200
-    mock_response.json.return_value = {"result":1}
-    assert transaction_summ(transact_list,"RUB")==2
+    mock_response.json.return_value = {"result":2}
+    assert transaction_summ(transaction,"RUB")==2
